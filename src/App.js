1aa78eea0ae098f344581c14a14c3d84ea9591f7
CamilleSylla <camillesylla.web@gmail.com>
2020-07-23 18:17:13 +0200

    added routes to the project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 const SignIn = lazy(() => import('./SignIn/SignIn'));
 const SignUp = lazy (() => import('./SignUp/SignUp'));
+const Dashboard = lazy (() => import('./Dashboard/Dashboard'))
 
 class App extends React.Component {
   constructor () {
@@ -22,6 +23,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={SignIn}/>
           <Route path="/signup" component={SignUp}/>
+          <Route path="/dashboard" component={Dashboard}/>
         </Switch>
       </Suspense>
     </Router>
