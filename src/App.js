89c735940e80f170471ba9eaa5ac806c8aa7d191
CamilleSylla <camillesylla.web@gmail.com>
2020-07-23 17:37:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const SignIn = lazy(() => import('./SignIn/SignIn'));
+const SignUp = lazy (() => import('./SignUp/SignUp'));
+
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state={
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Suspense fallback={<div>Chargement...</div>}>
+        <Switch>
+          <Route exact path="/" component={SignIn}/>
+          <Route path="/signup" component={SignUp}/>
+        </Switch>
+      </Suspense>
+    </Router>
+  );}
 }
 
 export default App;
