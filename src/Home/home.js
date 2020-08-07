efa27376429d23b735417309e8b89341fcb72ff8
CamilleSylla <camillesylla.web@gmail.com>
2020-08-07 17:13:43 +0200

    added header banniere and cards gotta center cards

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,11 +1,31 @@
 import React from "react";
-import NavBar from './Nav';
+import Header from './Header';
+import Banniere from './Banniere';
+import Cards from './Cards'
 
+const sections = [
+    { title: 'Accueil', url: '#' },
+    { title: 'Notre Offre', url: '#' },
+    { title: 'Qui Sommes Nous ?', url: '#' },
+    { title: 'Contact', url: '#' },
+    
+  ];
+
+  const bannierePost = {
+    title: 'Bienvenue sur Huge',
+    description:
+      "Votre solution pour la gestion de culture !",
+    image: 'https://source.unsplash.com/random',
+    imgText: 'main image description',
+    linkText: 'Continue reading…',
+  };
 class Home extends React.Component{
     render () {
         return ( 
             <div>
-            <NavBar/>
+            <Header title="Huge" sections={sections} />
+            <Banniere post={bannierePost} />
+            <Cards/>
         </div>
         )
         
