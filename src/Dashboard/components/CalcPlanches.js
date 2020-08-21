39c6afd1cd4c42cee11e60dd336eba92b08c0f0b
CamilleSylla <camillesylla.web@gmail.com>
2020-08-21 15:38:14 +0200

    render res in jsx

--- src/Dashboard/components/CalcPlanches.js
+++ src/Dashboard/components/CalcPlanches.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 import item from '../GlobalData';
 
@@ -12,13 +13,22 @@ const useStyles = theme =>({
         textAlign: "center",
     },
 });
+let planches;
+let lol;
+
 
 class Planches extends React.Component{
-    state=item;
+    state ={
+        show : "false"
+      }
+
+    parc=item;
     userXY= {
         x: '',
         y: ''
     };
+    
+    showRes;
     onLargeurChange = (event) => {
         this.userXY.x = event.target.value;
         console.log(this.userXY.x)
@@ -27,22 +37,36 @@ class Planches extends React.Component{
         this.userXY.y = event.target.value;
         console.log(this.userXY.y)
       }
+      
+      
+      
+      
+      
 
     render () {
-        console.log(this.state.production[0])
-        const t = this.state.production[0].total;
+        let lol;
+        if (this.state.show === "true") {
+    
+        const t = this.parc.production[0].total;
         const x = this.userXY.x;
         const y = this.userXY.y;
-        console.log(t, x ,y)
         const res = (( t /  (((x*100)*(y*100))/100)));
-        console.log(res)
-
+        lol = res;
+        console.log(lol)      
+        }
+        
+        
         
             return (
                 <Grid>
                     <TextField id="standard-basic" label="Longueur" onChange={this.onLongueurChange} />
                     <TextField id="standard-basic" label="Largeur" onChange={this.onLargeurChange} />
-                    {res}
+                    <Button variant="contained" color="primary" onClick={() => {this.setState({show: "true"}); 
+                         console.log(this.state.show);
+                         }}>
+                    Primary
+                    </Button>
+            {lol}
                 </Grid>
             )
         }
